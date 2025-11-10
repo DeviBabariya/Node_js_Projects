d40ebb2938a87629ad4e59e9236a6dba43b0b871
Devi Babariya <devibabariyarnw@gmail.com>
2025-11-10 23:03:40 +0530

    Passport-Js

---
+++ PR-7-Passport/config/DbConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://devibabariya:192006@cluster0.7xghalq.mongodb.net/otp");
+    // await mongoose.connect("");
+    console.log("Database Connected Successfully");
+  } catch (err) {
+    console.error("Database Connection Failed:", err);
+  }
+};
+
+module.exports = dbConnect;
